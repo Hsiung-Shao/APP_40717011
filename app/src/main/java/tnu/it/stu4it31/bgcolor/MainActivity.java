bd9845b63cd3a1435d4c97a85311ed21ca118d9f
XiongShao <jerry29221979@gmail.com>
2021-02-25 15:05:14 +0800

    Update button function
    Add set background color Button

--- app/src/main/java/tnu/it/stu4it31/bgcolor/MainActivity.java
+++ app/src/main/java/tnu/it/stu4it31/bgcolor/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnRed;
+    Button btnRed, btnGreen, btnBlue, btnYellow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +16,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnRed = findViewById(R.id.btnRed);
+        btnBlue = findViewById(R.id.btnBlue);
+        btnGreen = findViewById(R.id.btnGreen);
+        btnYellow = findViewById(R.id.btnYellow);
+
         btnRed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 View bg = findViewById(android.R.id.content);
                 bg.setBackgroundColor(Color.RED);
+            }
+        });
+        btnBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View bg = findViewById(android.R.id.content);
+                bg.setBackgroundColor(Color.BLUE);
+            }
+        });
+        btnGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View bg = findViewById(android.R.id.content);
+                bg.setBackgroundColor(Color.GREEN);
+            }
+        });
+        btnYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View bg = findViewById(android.R.id.content);
+                bg.setBackgroundColor(Color.YELLOW);
             }
         });
     }
